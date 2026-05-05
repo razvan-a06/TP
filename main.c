7d375c2628cc1b9ba5b1209096d2fa1d8509f920
Razvan Ardel <razvanardel65@gmail.com>
2026-05-05 21:49:41 +0300

    Adaugat nivele de dificultate + fisiere json cu cuvinte

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#define COLOR_RED     "\033[1;31m"
+#define COLOR_GREEN   "\033[1;32m"
+#define COLOR_YELLOW  "\033[1;33m"
+#define COLOR_CYAN    "\033[1;36m"
+#define COLOR_RESET   "\033[0m"
+
+#define CHUNK 50
 
 void clearScreen() {
 #ifdef _WIN32
@@ -20,7 +30,7 @@ void wait(int seconds) {
 
 void afisareMeniu() {
     printf("==============================\n");
-    printf("      JOC SPANZURATOAREA      \n");
+    printf(COLOR_CYAN"      JOC SPANZURATOAREA      \n"COLOR_RESET);
     printf("==============================\n");
     printf(" 1. Joaca\n");
     printf(" 2. Reguli\n");
@@ -100,17 +110,106 @@ void drawHangman(int mistakes) {
     }
 }
 
-void rebuildWord(char c, char* word, char* hiddenWord) {
+void rebuildWord(char c, const char* word, char* hiddenWord) {
     int length = strlen(word);
     for (int i=0; i<length; i++)
         if (c == word[i])
             hiddenWord[i] = c;
 }
 
-void playGame() {
-    char secretWord[] = "joc";
+void freeWords(char** dictionar, int count) {
+    for (int i=0; i<count; i++)
+        free(dictionar[i]);
+}
+
+char** loadWordsFromFile(const char* filename, int* count) {
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+        printf("eroare la deschiderea fisierului!");
+        exit(-1);
+    }
+
+    int size = CHUNK;
+    char** dictionar = malloc(size*sizeof(char*));
+    if (!dictionar) {
+        printf("Memorie insuficienta!");
+        fclose(f);
+        exit(-1);
+    }
+    *count = 0;
+    char line[100] = "";
+    while (fgets(line, sizeof(line), f)) {
+        if (*count == size) {
+            size += CHUNK;
+            char** temp = realloc(dictionar, size*sizeof(char*));
+            if (!temp) {
+                printf("Memorie insuficienta!");
+                fclose(f);
+                freeWords(dictionar, *count);
+                free(dictionar);
+                exit(-1);
+            }
+            dictionar = temp;
+        }
+        char* start = strchr(line, '"');
+        if (start) {
+            start++;
+            char word[100] = "";
+            int i=0;
+            while (*start != '"') {
+                word[i++] = (*start);
+                start++;
+            }
+            word[i] = '\0';
+            dictionar[*count] = malloc((strlen(word)+1) * sizeof(char));
+            if (!dictionar[*count]) {
+                printf("Memorie insuficienta!");
+                fclose(f);
+                freeWords(dictionar, *count);
+                free(dictionar);
+                exit(-1);
+            }
+            strcpy(dictionar[(*count)++], word);
+        }
+    }
+    if (fclose(f) != 0) {
+        printf("Eroare inchidere fisier!");
+        freeWords(dictionar, *count);
+        free(dictionar);
+        exit(-1);
+    }
+    return dictionar;
+}
+
+void playGame(int diff) {
+    printf("\n---> Incepem un joc nou!\n");
+    wait(2);
+    char** dictionar; int count;
+    switch (diff) {
+        case 1:
+            dictionar = loadWordsFromFile("../usor.json", &count);
+            break;
+        case 2:
+            dictionar = loadWordsFromFile("../mediu.json", &count);
+            break;
+        case 3:
+            dictionar = loadWordsFromFile("../greu.json", &count);
+            break;
+        default:
+            printf("eroare dificultate");
+            exit(-1);
+    }
+    int index = rand() % count;
+
+    char* secretWord = dictionar[index];
     int length = strlen(secretWord);
     char* hiddenWord = malloc((length+1)*sizeof(char));
+    if (!hiddenWord) {
+        printf("Memorie insuficienta!");
+        freeWords(dictionar, count);
+        free(dictionar);
+        exit(-1);
+    }
     char wrongLetters[7] = "";
 
     for(int i = 0; i < length; i++) {
@@ -125,7 +224,7 @@ void playGame() {
     while (mistakes < maxMistakes && win == 0) {
         clearScreen();
         drawHangman(mistakes);
-        printf("Greseli: %d / %d\n\n", mistakes, maxMistakes);
+        printf(COLOR_RED"Greseli: %d / %d" COLOR_RESET "\n\n", mistakes, maxMistakes);
 
         printf("Cuvantul: ");
         for(int i = 0; i < length; i++) {
@@ -133,17 +232,34 @@ void playGame() {
         }
         printf("\n\n");
 
-        printf("Litere gresite incercate: ");
+        printf(COLOR_RED "Litere gresite incercate: ");
         for (int i=0; i<mistakes; i++)
             printf("%c ", wrongLetters[i]);
-        printf("\n\n");
+        printf(COLOR_RESET"\n\n");
 
         char guess;
-        printf("Introdu o litera: ");
-        scanf(" %c", &guess);
+        int valid;
+        do {
+            printf("Introdu o litera: ");
+            scanf(" %c", &guess);
+            char nextchar = getchar();
+            if (nextchar != '\n') {
+                printf("\n" COLOR_YELLOW "EROARE: Te rog sa introduci O SINGURA litera!" COLOR_RESET "\n\n");
+                while (getchar() != '\n');
+                valid = 0;
+            }
+            else if (!isalpha(guess)) {
+                printf("\n" COLOR_YELLOW "EROARE: Ai introdus o cifra sau un simbol! Te rog introdu doar litere (a-z)." COLOR_RESET "\n\n");
+                valid = 0;
+            }
+            else {
+                guess = tolower(guess);
+                valid = 1;
+            }
+        } while (valid == 0);
         printf("\n\n");
         if (strchr(hiddenWord, guess)) {
-            printf("Ai ghicit deja aceasta litera!\n");
+            printf(COLOR_YELLOW"Ai ghicit deja aceasta litera!" COLOR_RESET "\n");
             wait(2);
         }
         else if (strchr(secretWord, guess)) {
@@ -151,7 +267,7 @@ void playGame() {
         }
         else
             if (strchr(wrongLetters,guess)) {
-                printf("Ai incercat deja aceasta litera!");
+                printf(COLOR_YELLOW"Ai incercat deja aceasta litera!" COLOR_RESET);
                 wait(2);
             }
             else {
@@ -169,18 +285,23 @@ void playGame() {
     drawHangman(mistakes);
     if (win == 1) {
         clearScreen();
-        printf("\nFELICITARI! Ai ghicit cuvantul: %s\n", secretWord);
+        printf("\n" COLOR_GREEN "FELICITARI! Ai ghicit cuvantul: %s" COLOR_RESET "\n", secretWord);
         wait(2);
+        clearScreen();
     }
     else {
-        printf("\nAI PIERDUT! Cuvantul corect era: %s\n", secretWord);
+        printf("\n" COLOR_RED "AI PIERDUT! Cuvantul corect era: %s" COLOR_RESET "\n", secretWord);
         wait(2);
+        clearScreen();
     }
     free(hiddenWord);
+    freeWords(dictionar, count);
+    free(dictionar);
 }
 
 int main() {
-    int optiune;
+    srand(time(NULL));
+    int optiune, diff;
 
     do {
         clearScreen();
@@ -193,9 +314,31 @@ int main() {
 
         switch(optiune) {
             case 1:
-                printf("\n---> Incepem un joc nou!\n");
+                do {
+                clearScreen();
+                printf("---> Alegeti nivelul de dificultate:\n\n");
+                printf("1. Usor\n2. Mediu\n3. Greu\n\nAlege o optiune:");
+                    if (scanf("%d", &diff) != 1) {
+                        diff = -1;
+                    }
+                    while (getchar() != '\n');
+                    switch (diff) {
+                        case 1:
+                            playGame(1);
+                            break;
+                        case 2:
+                            playGame(2);
+                            break;
+                        case 3:
+                            playGame(3);
+                            break;
+                        default:
+                            printf("\n---> Optiune invalida! Te rog alege 1, 2 sau 3.\n");
+                            diff = -1;
+                            wait(2);
+                    }
+                } while (diff < 0);
                 wait(2);
-                playGame();
                 optiune = 0;
                 break;
             case 2:
